32276e98ed29a58e72f949e49c362b88b30e646d
lilou <leilaphebidias@gmail.com>
2020-03-28 23:17:37 +0100

    First Commit

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import Nav from "../components/nav"
+import TableUser from "../components/tabUser"
+
+const Administration= () => {
+  return (
+    <div>
+      <Nav />
+      <center><h2>Tableau des utilisateurs</h2></center>
+      <TableUser />
+    </div>
+  )
+}
+export default Administration 
